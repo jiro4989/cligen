4daa41af4301241353bd656db6b6277a7baccc5f
jiro4989 <jiroron666@gmail.com>
2020-05-30 16:26:49 +0000

    Move CSS to docs directory

--- doc.sh
+++ doc.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 for m in cligen cligen/parseopt3 cligen/argcvt; do
   if [ docs/$(basename $m).html -nt $m.nim ]; then
-      echo $m html is up-to-date
-      continue
+    echo $m html is up-to-date
+    continue
   fi
   nim doc $m
   mv $(basename $m).html docs
+  mv nimdoc.out.css docs
 done
